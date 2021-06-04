61ffb2f7d89b9cb104f9bd7b1de9caa422dc39e7
Ethan Smith <ethan@onebytegone.com>
2021-06-04 17:54:09 -0400

    feat: work to unify mac/win navigation

--- keyboards/redox/keymaps/onebytegone/keymap.c
+++ keyboards/redox/keymaps/onebytegone/keymap.c
@@ -1,22 +1,35 @@
 #include QMK_KEYBOARD_H
 
 #define _QWERTY_MAC 0
-#define _QWERTY_WIN 1
-#define _NAV 2
+#define _NAV_MAC 1
+#define _QWERTY_WIN 2
+#define _NAV_WIN 3
 
 enum custom_keycodes {
   QWERTY_MAC = SAFE_RANGE,
+  NAV_MAC,
   QWERTY_WIN,
-  NAV
+  NAV_WIN,
 };
 
 #define CK_DSKL LCTL(KC_LEFT) // macOS to left desktop
 #define CK_DSKR LCTL(KC_RIGHT)  // macOS to right desktop
 
-#define MO_NAV MO(_NAV)
+#define MO_NAVM MO(_NAV_MAC)
+#define MO_NAVW MO(_NAV_WIN)
 
 #define DF_MAC DF(_QWERTY_MAC)
 #define DF_WIN DF(_QWERTY_WIN)
+
+// (B)eginning (O)f (L)ine, (E)nd (O)f (L)ine
+#define MAC_BOL LGUI(KC_LEFT)
+#define MAC_EOL LGUI(KC_RIGHT)
+
+// (B)eginning (O)f (W)ord, (E)nd (O)f (W)ord, (B)eginning of (N)ext (W)ord
+#define MAC_BOW LALT(KC_LEFT)
+#define MAC_EOW LALT(KC_RIGHT)
+#define WIN_BOW LCTL(KC_LEFT)
+#define WIN_BNW LCTL(KC_RIGHT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -28,9 +41,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_ESC  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_MINUS,                          KC_EQUAL,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼========┼────────┐       ┌────────┼========┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_PGUP ,KC_PGDN ,        KC_HOME ,KC_END  ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
+     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_PGUP ,KC_PGDN ,        MAC_BOL ,MAC_EOL ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_LCTRL,KC_LCTRL,KC_LCTRL,KC_LALT ,     KC_LGUI ,    KC_SPC  ,KC_BSPC ,        KC_ENT  ,KC_SPC  ,    MO_NAV  ,     KC_RGUI ,KC_RALT ,KC_RCTRL,XXXXXXX
+     KC_LCTRL,KC_LCTRL,KC_LCTRL,KC_LALT ,     KC_LGUI ,    KC_SPC  ,KC_BSPC ,        KC_ENT  ,KC_SPC  ,    MO_NAVM ,     KC_RGUI ,KC_RALT ,KC_RCTRL,XXXXXXX
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  ),
+
+  [_NAV_MAC] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     DF_WIN  ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,DF_MAC  ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,MAC_EOW ,XXXXXXX ,XXXXXXX ,KC_F11  ,                          KC_F12  ,CK_DSKL ,XXXXXXX ,XXXXXXX ,CK_DSKR ,XXXXXXX ,KC_VOLU ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,XXXXXXX ,KC_VOLD ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼========┼────────┐       ┌────────┼========┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,MAC_BOW ,XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_MPRV ,KC_MNXT ,KC_MPLY ,KC_MUTE ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     _______ ,_______ ,_______ ,_______ ,     _______ ,    XXXXXXX ,KC_DEL  ,        XXXXXXX ,XXXXXXX ,    _______ ,     _______ ,_______ ,_______ ,_______
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
@@ -44,19 +71,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼========┼────────┐       ┌────────┼========┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_PGUP ,KC_PGDN ,        KC_HOME ,KC_END  ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_LGUI, KC_LGUI, KC_LGUI, KC_LALT ,     KC_LCTRL,    KC_SPC  ,KC_BSPC ,        KC_ENT  ,KC_SPC  ,    MO_NAV  ,     KC_RCTRL,KC_RALT ,KC_RGUI ,XXXXXXX
+     KC_LGUI, KC_LGUI, KC_LGUI, KC_LALT ,     KC_LCTRL,    KC_SPC  ,KC_BSPC ,        KC_ENT  ,KC_SPC  ,    MO_NAVW ,     KC_RCTRL,KC_RALT ,KC_RGUI ,XXXXXXX
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
-  [_NAV] = LAYOUT(
+  [_NAV_WIN] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      DF_WIN  ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,DF_MAC  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_F11  ,                          KC_F12  ,CK_DSKL ,XXXXXXX ,XXXXXXX ,CK_DSKR ,XXXXXXX ,KC_VOLU ,
+     XXXXXXX ,XXXXXXX ,WIN_BNW ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_F11  ,                          KC_F12  ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_VOLU ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,XXXXXXX ,KC_VOLD ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼========┼────────┐       ┌────────┼========┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_MPRV ,KC_MNXT ,KC_MPLY ,KC_MUTE ,
+     _______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,WIN_BOW ,XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_MPRV ,KC_MNXT ,KC_MPLY ,KC_MUTE ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
      _______ ,_______ ,_______ ,_______ ,     _______ ,    XXXXXXX ,KC_DEL  ,        XXXXXXX ,XXXXXXX ,    _______ ,     _______ ,_______ ,_______ ,_______
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘

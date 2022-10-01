7ad9561e07424a75c1127b158435f2d2cf0793e9
Ethan Smith <ethan@onebytegone.com>
2022-10-01 16:10:45 -0400

    feat: fill out game layer

--- keyboards/crkbd/keymaps/onebytegone/keymap.c
+++ keyboards/crkbd/keymaps/onebytegone/keymap.c
@@ -52,7 +52,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_BASE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -101,13 +101,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L_MC] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-         KC_1,    KC_2, XXXXXXX,    KC_W,    KC_E, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      MC,\
+         KC_1,    KC_2,    KC_Q,    KC_W,    KC_E,    KC_R,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     MC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         KC_3,  KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+         KC_3,  KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,    KC_Z,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+         KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,    KC_X,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL, KC_LSFT,  KC_SPC,    XXXXXXX, XXXXXXX, XXXXXXX \
+                                          KC_LCTL, KC_LSFT,  KC_SPC,       KC_C,    KC_V,    KC_B \
                                       //`--------------------------'  `--------------------------'
 
   )
